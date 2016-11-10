994b38992ef8a44b6411e556c64c708b06989b22
alvarotellez <alvarotellezhidalgo@gmail.com>
2016-11-10 11:05:44 +0100

    Falta NotFoundController

--- Controller/LibroController.php
+++ Controller/LibroController.php
@@ -8,5 +8,34 @@
  */
 class LibroController
 {
+    public function manageGetVerb(Request $request)
+    {
 
+        $listaLibros = null;
+        $id = null;
+        $response = null;
+        $code = null;
+
+        if (isset($request->getUrlElements()[2])) {
+            $id = $request->getUrlElements()[2];
+        }
+
+
+        $listaLibros = LibroHandlerModel::getLibro($id);
+
+        if ($listaLibros != null) {
+            $code = '200';
+
+        } else {
+
+
+            if (LibroHandlerModel::isValid($id)) {
+                $code = '404';
+            }
+
+        }
+
+        $response = new Response($code, null, $listaLibros, $request->getAccept());
+        $response->generate();
+    }
 }
