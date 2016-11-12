5f0797f5a7a955d068a8c2acbb1e5d94d69bdedc
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2016-11-12 08:29:43 +0100

    Request terminado

--- Request.php
+++ Request.php
@@ -10,6 +10,9 @@ class Request
 {
     private $url_elements;
     private $query_string;
+
+
+
     private $verb;
     private $body_parameters;
     private $format;
@@ -21,19 +24,129 @@ class Request
         $this->url_elements = $url_elemnts;
         $this->query_string = $query_string;
         $this->parseBody($body,$content_type);
-
+        //Para decirle que tipo de datos acepta nuestro servicio
         switch($accept){
             case 'application/json':
             case'*/*':
             case null:
                 $this->accept = 'json';
                 break;
-            case 'application/xml':
-                $this->accept ='xml';
-                break;
             default:
                 $this->accept = 'unsupported';
                 break;
         }
+        return true;
     }
-}
+    //Cuerpo del json
+    private function parseBody($body,$content_type)
+    {
+        $parameters = array();
+
+        switch ($content_type) {
+            case "application/json":
+                $this->format = "json";
+                $parameters = json_decode($body);
+                $body_params = json_decode($body);
+                break;
+            default:
+                break;
+            $this->body_parameters = $parameters;
+                }
+        }
+        /**
+         * @return mixed
+         */
+        public function getUrlElements()
+        {
+            return $this->url_elements;
+        }
+
+        /**
+         * @param mixed $url_elements
+         */
+        public function setUrlElements($url_elements)
+        {
+            $this->url_elements = $url_elements;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getQueryString()
+        {
+            return $this->query_string;
+        }
+
+        /**
+         * @param mixed $query_string
+         */
+        public function setQueryString($query_string)
+        {
+            $this->query_string = $query_string;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getVerb()
+        {
+            return $this->verb;
+        }
+
+        /**
+         * @param mixed $verb
+         */
+        public function setVerb($verb)
+        {
+            $this->verb = $verb;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getBodyParameters()
+        {
+            return $this->body_parameters;
+        }
+
+        /**
+         * @param mixed $body_parameters
+         */
+        public function setBodyParameters($body_parameters)
+        {
+            $this->body_parameters = $body_parameters;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getFormat()
+        {
+            return $this->format;
+        }
+
+        /**
+         * @param mixed $format
+         */
+        public function setFormat($format)
+        {
+            $this->format = $format;
+        }
+
+        /**
+         * @return string
+         */
+        public function getAccept()
+        {
+            return $this->accept;
+        }
+
+        /**
+         * @param string $accept
+         */
+        public function setAccept($accept)
+        {
+            $this->accept = $accept;
+        }
+
+    }
